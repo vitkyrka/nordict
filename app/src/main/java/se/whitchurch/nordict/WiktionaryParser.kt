6b9fe757ded068e7dacb13f82372673e06189981
Vincent Whitchurch <vincent@whitchurch.se>
2021-06-27 18:51:02 +0200

    Improve Wiktionary parsing

--- app/src/main/java/se/whitchurch/nordict/WiktionaryParser.kt
+++ app/src/main/java/se/whitchurch/nordict/WiktionaryParser.kt
@@ -1,14 +1,22 @@
 package se.whitchurch.nordict
 
 import android.net.Uri
+import android.util.Log
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class WiktionaryParser {
     companion object {
         private fun parseDefinition(headword: Word, li: Element) {
-            val def = Word.Definition("", li)
+            val clone = li.clone()
+
+            clone.select("ul")?.forEach {
+                it.remove()
+            }
+
+            val def = Word.Definition(clone.text(), li)
             headword.definitions.add(def)
+            li.remove()
         }
 
         fun parse(page: String, uri: Uri, tag: String, shortName: String): Word? {
@@ -16,7 +24,7 @@ class WiktionaryParser {
 
             val heading = doc.selectFirst("#section_0") ?: return null
             val word = heading.text()
-            val element = doc.selectFirst("#bodyContent")
+            val element = doc.selectFirst("#content")
             val content = element.outerHtml()
             val cleanpage = doc.head().html() + "<body>" + content
 
@@ -27,6 +35,39 @@ class WiktionaryParser {
                     element,
                     doc.head().html() + "<body>")
 
+            // Remove other languages
+            var preserve = false
+            element.selectFirst(".mw-parser-output")?.children()?.forEach {
+                if (preserve) {
+                    it.addClass("open-block")
+                } else {
+                    it.remove()
+                }
+
+                preserve = false;
+
+                // <span class="sectionlangue" id="fr">Français</span>
+                it.selectFirst("#$shortName")?.let {
+                    preserve = true
+                }
+            }
+
+            // Remove translations
+            preserve = true
+            element.selectFirst("section")?.children()?.forEach {
+                it.selectFirst("span.mw-headline")?.let { headline ->
+                    Log.i("foo", headline.id())
+                    if (headline.id().startsWith("Traductions")) {
+                        it.remove()
+                        preserve = false;
+                        return@forEach
+                    }
+                }
+
+                if (it.tagName() != "div") preserve = true
+                if (!preserve) it.remove()
+            }
+
             doc.select("audio")?.forEach audio@{ audio ->
                 val source = audio.selectFirst("source") ?: return@audio
                 val url = source.attr("src")
@@ -34,7 +75,7 @@ class WiktionaryParser {
                 headword.audio.add("https:$url")
             }
 
-            element.selectFirst("ol")?.select("li")?.forEach {
+            element.selectFirst("ol")?.children()?.forEach {
                 parseDefinition(headword, it)
             }
 
